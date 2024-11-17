383d6d0177f26533a210ed2b62616471d8e452fb
margonevolina <margo6984@gmail.com>
2024-11-17 17:42:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("1st program")
+print(9**0.5*5)
+print("2st program")
+print(9.99>9.98 and 1000!=1000.1)
+print("3st program")
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+print("4st program")
+print(int(float("123.456")*10)%10)
